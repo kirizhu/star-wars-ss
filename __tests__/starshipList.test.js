d806e29103b7d87b650ba868a15abe437eb8a5cb
Eddy Garcia <32507458+kirizhu@users.noreply.github.com>
2024-02-26 20:05:18 +0100

    small refactor to test

--- __tests__/starshipList.test.js
+++ __tests__/starshipList.test.js
@@ -3,10 +3,13 @@ import { render } from '@testing-library/react-native';
 import StarshipList from '../src/components/StarshipList/StarshipList.component';
 
 describe('StarshipList', () => {
-  it('renders StarshipList', () => {
-    const { getByTestId } = render(<StarshipList error={false} loading={false}/>);
+  it('renders empty StarshipList', () => {
+    const { getByTestId , getByText} = render(<StarshipList error={false} loading={false}/>);
     const list = getByTestId('ship-list');
+    const emptyList =getByText(`These are not the starships 
+    you are looking for...`)
     expect(list).toBeTruthy();
+    expect(emptyList).toBeTruthy();
   });
   it('renders ErrorComponent when error is true', () => {
     const { getByText } = render(<StarshipList error={true} loading={false} />);
